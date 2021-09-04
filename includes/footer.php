1ff12ccb856fddc712a18e076ab0d9117e998567
Wasif <wasifanton@gmail.com>
2021-09-04 12:41:35 +0300

    Small change in footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 <footer class="py-3 my-4">
     <ul class="nav justify-content-center border-bottom pb-3 mb-3"></ul>
-    <p class="text-center text-muted">© 2021 <span class="buy">Buy</span><span class="shop">Shop</span>, Inc</p>
+    <p class="text-center text-muted">Made with ❤️ &nbsp © 2021 <span class="buy">Buy</span><span class="shop">Shop</span>, Inc</p>
 </footer>
 
 <!-- Link to js Bootstrap -->
