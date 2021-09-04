70e6ffec83d13f8e03e7fd0203ba798ee6ee61c2
Wasif <wasifanton@gmail.com>
2021-09-04 12:28:39 +0300

    Small Changes
    - Adding to header.php
    - Adding to footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,23 +1,10 @@
-<!DOCTYPE html>
-<html lang="en">
+<footer class="py-3 my-4">
+    <ul class="nav justify-content-center border-bottom pb-3 mb-3"></ul>
+    <p class="text-center text-muted">© 2021 <span class="buy">Buy</span><span class="shop">Shop</span>, Inc</p>
+</footer>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Bootstrap -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-    <title></title>
-</head>
-
-<body>
-    <footer class="py-3 my-4">
-        <ul class="nav justify-content-center border-bottom pb-3 mb-3"></ul>
-        <p class="text-center text-muted">© 2021 <span class="buy">Buy</span><span class="shop">Shop</span>, Inc</p>
-    </footer>
-
-    <!-- Link to js Bootstrap -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
+<!-- Link to js Bootstrap -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -1,100 +1,81 @@
-<!DOCTYPE html>
-<html lang="en">
+<!-- Header - Dynamic Title -->
+<?php
+$titel = 'Home Page';
+require_once 'includes/header.php'
+?>
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <!-- Link to css Bootstrap -->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-  <link rel="stylesheet" type="text/css" href="css/style.css">
-</head>
+<!-- Nav Bar -->
+<nav class="navbar navbar-expand-lg navbar-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="index.php"><span class="buy">Buy</span><span class="shop">Shop</span></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      </ul>
+      <!-- Login, Sign-up -->
+      <form class="d-flex">
+        <button type="button" class="btn btn-outline-warning me-2">Login</button>
+        <button type="button" class="btn btn-warning">Sign-up</button>
+      </form>
+    </div>
+  </div>
+</nav>
 
-<body>
-  <!-- Header - Dynamic Title -->
-  <?php
-  $titel = 'Home Page';
-  require_once 'includes/header.php'
-  ?>
-
-  <!-- Nav Bar -->
-  <nav class="navbar navbar-expand-lg navbar-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="index.php"><span class="buy">Buy</span><span class="shop">Shop</span></a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        </ul>
-        <!-- Login, Sign-up -->
-        <form class="d-flex">
-          <button type="button" class="btn btn-outline-warning me-2">Login</button>
-          <button type="button" class="btn btn-warning">Sign-up</button>
-        </form>
+<!-- Welcome -->
+<section class="pt-4 pt-md-11">
+  <div class="container">
+    <div class="row align-items-center">
+      <div class="col-12 col-md-5 col-lg-6 order-md-2">
+        <!-- Right Image -->
+        <img src="img/main_page/img1.jpeg" class="rounded img-fluid mw-md-150 mw-lg-130 mb-6 mb-md-0 aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
+      </div>
+      <div class="col-12 col-md-7 col-lg-6 order-md-1 aos-init aos-animate" data-aos="fade-up">
+        <!-- Heading -->
+        <h1 class="display-3 text-center text-md-start">
+          Welcome to <span class="buy">Buy</span><span class="shop">Shop</span>.<br>
+          <span class="buy">Buy</span> anything.
+        </h1>
+        <!-- Text -->
+        <p class="lead text-center text-md-start text-muted mb-6 mb-lg-8">
+          <span class="buy">Buy</span> it with Love.
+        </p>
       </div>
     </div>
-  </nav>
+  </div>
+</section>
 
-  <!-- Welcome -->
-  <section class="pt-4 pt-md-11">
-    <div class="container">
-      <div class="row align-items-center">
-        <div class="col-12 col-md-5 col-lg-6 order-md-2">
-          <!-- Right Image -->
-          <img src="img/main_page/img1.jpeg" class="rounded img-fluid mw-md-150 mw-lg-130 mb-6 mb-md-0 aos-init aos-animate" data-aos="fade-up" data-aos-delay="100">
-        </div>
-        <div class="col-12 col-md-7 col-lg-6 order-md-1 aos-init aos-animate" data-aos="fade-up">
-          <!-- Heading -->
-          <h1 class="display-3 text-center text-md-start">
-            Welcome to <span class="buy">Buy</span><span class="shop">Shop</span>.<br>
-            <span class="buy">Buy</span> anything.
-          </h1>
-          <!-- Text -->
-          <p class="lead text-center text-md-start text-muted mb-6 mb-lg-8">
-            <span class="buy">Buy</span> it with Love.
-          </p>
-        </div>
-      </div>
+&nbsp
+
+<!-- Images -->
+<section class="container-fluid pb-5 px-0">
+  <!-- On large screen we have 3 columns of imgs, On medium and small screen we have 2 colums of imgs-->
+  <div class="row p-0">
+    <!-- First Row - -->
+    <div class="col-lg-4 col-sm-6 p-0">
+      <img class="rounded img-fluid" src="https://images.unsplash.com/photo-1522204523234-8729aa6e3d5f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" class="img-thumbnail">
     </div>
-  </section>
-
-  &nbsp
-
-  <!-- Images -->
-  <section class="container-fluid pb-5 px-0">
-    <!-- On large screen we have 3 columns of imgs, On medium and small screen we have 2 colums of imgs-->
-    <div class="row p-0">
-      <!-- First Row - -->
-      <div class="col-lg-4 col-sm-6 p-0">
-        <img class="rounded img-fluid" src="https://images.unsplash.com/photo-1522204523234-8729aa6e3d5f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" class="img-thumbnail">
-      </div>
-      <div class="col-lg-4 col-sm-6 p-0">
-        <img class="rounded img-fluid" src="https://images.unsplash.com/photo-1563013544-824ae1b704d3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" class="img-thumbnail">
-      </div>
-      <div class="col-lg-4 col-sm-6 p-0">
-        <img class="rounded img-fluid" src="https://images.unsplash.com/photo-1542744095-291d1f67b221?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" class="img-thumbnail">
-      </div>
-      <!-- Second Row - -->
-      <div class="col-lg-4 col-sm-6 p-0">
-        <img class="rounded img-fluid" src="https://images.unsplash.com/photo-1483985988355-763728e1935b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" class="img-thumbnail">
-      </div>
-      <div class="col-lg-4 col-sm-6 p-0">
-        <img class="rounded img-fluid" src="https://images.unsplash.com/photo-1555529669-e69e7aa0ba9a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" class="img-thumbnail">
-      </div>
-      <div class="col-lg-4 col-sm-6 p-0">
-        <img class="rounded img-fluid" src="https://images.unsplash.com/photo-1484807352052-23338990c6c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" class="img-thumbnail">
-      </div>
+    <div class="col-lg-4 col-sm-6 p-0">
+      <img class="rounded img-fluid" src="https://images.unsplash.com/photo-1563013544-824ae1b704d3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" class="img-thumbnail">
     </div>
-  </section>
+    <div class="col-lg-4 col-sm-6 p-0">
+      <img class="rounded img-fluid" src="https://images.unsplash.com/photo-1542744095-291d1f67b221?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" class="img-thumbnail">
+    </div>
+    <!-- Second Row - -->
+    <div class="col-lg-4 col-sm-6 p-0">
+      <img class="rounded img-fluid" src="https://images.unsplash.com/photo-1483985988355-763728e1935b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" class="img-thumbnail">
+    </div>
+    <div class="col-lg-4 col-sm-6 p-0">
+      <img class="rounded img-fluid" src="https://images.unsplash.com/photo-1555529669-e69e7aa0ba9a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" class="img-thumbnail">
+    </div>
+    <div class="col-lg-4 col-sm-6 p-0">
+      <img class="rounded img-fluid" src="https://images.unsplash.com/photo-1484807352052-23338990c6c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80" class="img-thumbnail">
+    </div>
+  </div>
+</section>
 
-  <!-- Footer -->
-  <?php
-  require_once 'includes/footer.php'
-  ?>
-
-  <!-- Link to js Bootstrap -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
-</body>
-
-</html>
+<!-- Footer -->
+<?php
+require_once 'includes/footer.php'
+?>
