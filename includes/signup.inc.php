ff53241c9fcddebb693a18a65cf617e5f6afeca9
Wasif <wasifanton@gmail.com>
2021-09-19 00:09:59 +0300

    Add and Change
    - Added some comments
    - Change in createUser

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -9,45 +9,56 @@ if (isset($_POST["submit"])) {
     $phone = $_POST["phone"];
     $region = $_POST["region"];
 
-    require_once 'darabase.inc.php';
+    require_once 'database.inc.php';
     require_once 'functions.inc.php';
 
     // Error Handler
-
     // If the fields are empty
-    // Function check if fields are empty
+
+    // emptyInputSignup function check if fields are empty (require_once 'functions.inc.php';)
     if (emptyInputSignup($id, $name, $email, $password, $confirm_password, $phone, $region) !== false) {
         header("location: ../signup.php?error=emptyinput");
         exit();
     }
+
+    // invalidId function check if the id is invalid
     if (invalidId($id) !== false) {
         header("location: ../signup.php?error=invalidid");
         exit();
     }
+
+    // invalidName function check if the name is invalid
     if (invalidName($name) !== false) {
         header("location: ../signup.php?error=invalidname");
         exit();
     }
+
+    // invalidEmail function check if the email is invalid
     if (invalidEmail($email) !== false) {
         header("location: ../signup.php?error=invalidemail");
         exit();
     }
-    // If password not match
+
+    // passwordMatch function check if the password are the same or not
     if (passwordMatch($password, $confirm_password) !== false) {
         header("location: ../signup.php?error=passworddontmatch");
         exit();
     }
+
+    // invalidPhone function check if the phone only numbers
     if (invalidPhone($phone) !== false) {
         header("location: ../signup.php?error=invalidphone");
         exit();
     }
-    // If user exists
+
+    // idExists function check if the user id exists
     if (idExists($conn, $id) !== false) {
         header("location: ../signup.php?error=usernametaken");
         exit();
     }
 
-    createUser($id, $name, $email, $password, $phone, $region);
+    // createUser function will create the user if it pass all the other functions
+    createUser($comm, $id, $name, $email, $password, $phone, $region);
 }
 //
 else {
