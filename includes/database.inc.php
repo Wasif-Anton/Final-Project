9b02e8fb187ad0a9df30b29d6842f89d3215f323
Wasif <wasifanton@gmail.com>
2021-09-19 00:08:46 +0300

    Changes
    Change in the $db_name

--- includes/database.inc.php
+++ includes/database.inc.php
@@ -4,7 +4,7 @@
 $server_name  = 'localhost';
 $db_username = "root";
 $db_password = '';
-$db_name = 'final_project_db';
+$db_name = 'project_db';
 
 $conn = new mysqli($server_name, $db_username, $db_password);
 
